258af2376ab3550e542b2c6787dfb652d00d2872
ZakhirOfTheVoid <78120444+ZakhirOfTheVoid@users.noreply.github.com>
2021-08-02 20:59:45 +0300

    Update generatorClass.kts

--- Kotlin/O.D.C./generatorClass.kts
+++ Kotlin/O.D.C./generatorClass.kts
@@ -1,10 +1,9 @@
-class UUIDGenerator():String{
+class UUIDGenerator(){
     val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
     var randomString: String = List(36) { charPool.random() }.joinToString("")
     var i:Int=0
     while (i<36){
         if (i==8 || i==13 || i==18 || i==23 ) randomString[i]="-"
         i++
-        println("${randomString[i]}")
     }
 }
